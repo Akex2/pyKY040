c0b1855088db23bf019ffede0d6961f7c67c0355
Akex2 <schointss@gmail.com>
2018-10-04 14:12:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='pyky040',
+    name='Rpi_Encoder',
     version='0.1.3',
     description='High-level interface for the KY040 rotary encoder and switch.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/raphaelyancey/pyKY040',
-    author='Raphael Yancey',
-    author_email='pypi@raphaelyancey.fr',
-    keywords='keyes rotary encoder switch ky040',
+    url='https://github.com/Akex2/Rpi_Encoder',
+    author='Alex',
+    author_email='schoints@gmail.com',
+    keywords='keyes rotary encoder switch A B',
     #py_modules=["pyky040"],
     packages=find_packages(),
     install_requires=['RPi.GPIO'],
     project_urls={
-        'Bug Reports': 'https://github.com/raphaelyancey/pyKY040/issues',
-        'Source': 'https://github.com/raphaelyancey/pyKY040',
+        'Bug Reports': 'https://github.com/Akex2/Rpi_Encoder/issues',
+        'Source': 'https://github.com/Akex2/Rpi_Encoder',
     },
 )
